bd7e708b9753db964539f9eaaa0d4b4b18c10869
RoseAppleMedia <michael@roseapplemedia.com>
2020-09-03 14:53:45 -0400

    added image processing

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,14 +50,14 @@ module.exports = {
       },
     },
 
-    // {
-    //   resolve: `gatsby-transformer-cloudinary`,
-    //   options: {
-    //     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    //     apiKey: process.env.CLOUDINARY_API_KEY,
-    //     apiSecret: process.env.CLOUDINARY_API_SECRET,
-    //     uploadFolder: "catty",
-    //   },
-    // },
+    {
+      resolve: `gatsby-transformer-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        uploadFolder: "catty",
+      },
+    },
   ],
 }

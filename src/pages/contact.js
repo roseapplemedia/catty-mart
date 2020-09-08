6bb4a95eff40620b4d645f9071b5d287f124c92e
RoseAppleMedia <michael@roseapplemedia.com>
2020-09-08 08:29:01 -0400

    added contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,36 @@
 import React from "react"
+import "../css/contact.css"
 
 export default function ContactPage() {
   return (
-    <div>
-      <h2>Welcome to Contact</h2>
+    <div className="container">
+      <form name="contact" method="POST" data-netlify="true">
+        <label for="name">
+          Name
+          <input type="text" id="name" name="name" placeholder="Your name.." />
+        </label>
+        <label for="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email.."
+          />
+        </label>
+
+        <label for="subject">
+          Message
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Message.."
+            // style="height: 200px"
+          ></textarea>
+        </label>
+        <div hidden data-netlify-recaptcha="true"></div>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   )
 }
